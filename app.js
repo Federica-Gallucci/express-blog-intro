ff4aab28d07060afa1fbbbaa867454addd10ac3c
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-05-12 16:43:11 +0200

    add rotta bacheca

--- app.js
+++ app.js
@@ -17,8 +17,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { posts } = require(`./index`);
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.json({ message: "Server del mio blog" });
+});
+
+app.get("/bacheca", (req, res) => {
+  res.json(posts);
 });
 
 app.listen(port, () => {
---
+++ index.js
@@ -0,0 +1,40 @@
+const posts = [
+  {
+    id: 1,
+    titolo: "Ciambellone",
+    contenuto: "",
+    immagine: "/img/ciambellone.jpeg",
+    tags: ["Ciambellone", "Torte da colazione"],
+  },
+  {
+    id: 1,
+    titolo: "Cracker alla barbabietola",
+    contenuto: "",
+    immagine: "/img/cracker_barbabietola.jpeg",
+
+    tags: ["salati", "barbabietola"],
+  },
+  {
+    id: 3,
+    titolo: "Pane fritto dolce",
+    contenuto: "",
+    immagine: "/img/pane_fritto_dolce.jpeg",
+    tags: ["dolce", "pane", "fritto"],
+  },
+  {
+    id: 4,
+    titolo: "Pasta barbabietola",
+    contenuto: "",
+    immagine: "/img/pasta_barbabietola.jpeg",
+    tags: ["Pasta", "barbabietola"],
+  },
+  {
+    id: 5,
+    titolo: "Torta paesana",
+    contenuto: "",
+    immagine: "/img/torta_paesana.jpeg",
+    tags: ["Torta", "Tradizioni"],
+  },
+];
+
+module.exports = { posts };
